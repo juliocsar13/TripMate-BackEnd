be390e930eafd09661846db7abff5095e3eff539
julcsar13 <julcsar13@gmail.com>
2018-02-07 13:20:50 -0500

    create relation bettwen costumer and post

--- common/models/post.js
+++ common/models/post.js
@@ -1,5 +1,33 @@
 'use strict';
+//const async = require('async')
 
-module.exports = function(Post) {
+module.exports = function(app) {
 
+//const mongoDs = Post.dataSources.mongoDs;
+
+/*var mongoDs = app.dataSources.mongoDs;
+async.parallel({
+	posts:async.apply(createPosts)
+},function(err,results){
+	if (err) throw err;
+	createPosts(results.posts,function(err){
+		console.log('> models created succesfully')
+	})
+})
+
+function createPosts(cb){
+	mongoDs.automigrate('Post',function(err){
+		if (err) return cb(err);
+		const Post = app.models.Post;
+		
+		Post.create([{
+			name:"aaaaaaa",
+			description:"bbbbbbbb",
+			destination:"cccccccc",
+			schedule:"ddddddddd"
+		}],cb)
+	})
+}*/
+
+//Post.remoteMethod()
 };
