dd34b92624afdc4f6d037a5852db86fb9520c848
julcsar13 <julcsar13@gmail.com>
2018-02-22 23:06:28 -0500

    agregar imagenes en el modelo post

--- common/models/post.js
+++ common/models/post.js
@@ -1,10 +1,44 @@
 'use strict';
 const elasticsearch = require('elasticsearch')
+const filestack = require('filestack-js');
+const request = require('request');
+
 module.exports = function(Post) {
+
+   
+      Post.observe('before save',(context,next)=>{
+          console.log('ANTES DE GUARDAR',context.instance)
+          
+          next();
+      })
+      /*Post.observe('before delete', (context, next) => {
+        const Container = Post.app.models.Container;
+    
+        Post.findOne({where: context.where}, (error, post) => {
+          if (error) return next(error);
+    
+          const filename = post.filename;
+          Container.removeFile(BUCKET, filename, (error, reply) => {
+            if (error) {
+              return next(new Error(error));
+            }
+    
+
+
+            next();
+          });
+        });
+      });*/
+    
+      
+    
+
     let client  = new elasticsearch.Client({
         host:'localhost:9200'
     })
+
     Post.observe('after save',function(ctx,next){
+        console.log("dsadsdas")
         let myId = ctx.instance.id;
         delete ctx.instance.id;
         client.create({index:'post',type:'post',body:ctx.instance,id:JSON.stringify(myId)
@@ -29,6 +63,7 @@ module.exports = function(Post) {
             verb:'get'
         }
     })
+
     Post.search = function(text,cb){
 		client.search({index:'post', type:'post', q:text}).then(function(res) {
 			let results = []
@@ -40,6 +75,7 @@ module.exports = function(Post) {
 			throw new Error(err)
 		})
     }
+   
 }
 
 
