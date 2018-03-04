ed74dd2b135a1f0e8041d9aade11757ced553d5b
Angel Rodriguez Romero <aleangrodriguez123@gmail.com>
2018-03-03 20:35:46 -0500

    refactor

--- common/models/post.js
+++ common/models/post.js
@@ -1,44 +1,67 @@
-'use strict';
+
 const elasticsearch = require('elasticsearch')
-module.exports = function(Post) {
-    let client  = new elasticsearch.Client({
+
+module.exports = Post => {
+
+    const client  = new elasticsearch.Client({
         host:'localhost:9200'
     })
-    Post.observe('after save',function(ctx,next){
-        let myId = ctx.instance.id;
-        delete ctx.instance.id;
-        client.create({index:'post',type:'post',body:ctx.instance,id:JSON.stringify(myId)
-        }).then(function(res){
-            next()
-        },function(err){
-            throw new Error(err)
+
+
+    Post.observe('after save', async (ctx,next) => {
+        client.create({
+            index:'post',
+            type:'post',
+            body: ctx.instance,
+            id: ctx.instance.id.toString()
         })
+        next()
     })
+
+
     Post.observe('before delete',function(ctx,next){
-        client.delete({index:'post',type:'post',id:ctx.where.id	
-        }).then(function(resp) {		
-            next();	
-        }, function(err) {		
-            throw new Error(err);	
+        client.delete({
+            index:'post',
+            type:'post',
+            id:ctx.where.id	
         })
+        next()
     })
+
+
     Post.remoteMethod('search',{
-        accepts:{arg:'text', type:'string'},
-        returns:{arg:'results',type:'string'},
-        http:{
+        accepts: {
+            arg:'text', 
+            type:'string'
+        },
+        returns: {
+            arg:'results',
+            type:'string'
+        },
+        http: {
             verb:'get'
         }
     })
-    Post.search = function(text,cb){
-		client.search({index:'post', type:'post', q:text}).then(function(res) {
-			let results = []
-			res.hits.hits.forEach(function(h) {
-				results.push(h._source)
-			});
-			cb(null, results);
-		}, function(err) {
-			throw new Error(err)
-		})
+
+
+    Post.search = async (text,cb) => {
+
+        try {
+            const res = await client.search({
+                index:'post', 
+                type:'post', 
+                q: text
+            })
+    
+            let results = []
+            res.hits.hits.forEach((h) => {
+                results.push(h._source)
+            });
+    
+            return results           
+        } catch (error) {
+            throw new Error(err)
+        }
     }
 }
 
