80e990f11acfff3391ad5c645e3b3a66fc05c20d
julcsar13 <julcsar13@gmail.com>
2018-03-06 11:47:02 -0500

    merge

--- common/models/asset.js
+++ common/models/asset.js
@@ -1,7 +1,41 @@
 'use strict';
-const fs = require('fs');
-const formidable = require('formidable');
-const BUCKET = 'test';
 module.exports = function(Asset) {
-    
+    Asset.upload = function(ctx,options,cb){
+        if(!options) options = {};
+        ctx.req.params.container = 'common';
+        Asset.app.models.container.upload(ctx.req,ctx.result,options,function (err,fileObj) {
+            if(err) {
+                cb(err);
+            } else {
+                var fileInfo = fileObj.files.file[0];
+                Asset.create({
+                    name: fileInfo.name,
+                    type: fileInfo.type,
+                    container: fileInfo.container,
+                    url: CONTAINERS_URL+fileInfo.container+'/download/'+fileInfo.name
+                },function (err,obj) {
+                    if (err !== null) {
+                        cb(err);
+                    } else {
+                        cb(null, obj);
+                    }
+                });
+            }
+        });
+    }
+
+    Asset.remoteMethod(
+        'upload',
+        {
+            description: 'Uploads a file',
+            accepts: [
+                { arg: 'ctx', type: 'object', http: { source:'context' } },
+                { arg: 'options', type: 'object', http:{ source: 'query'} }
+            ],
+            returns: {
+                arg: 'fileObject', type: 'object', root: true
+            },
+            http: {verb: 'post'}
+        }
+    );
 };
